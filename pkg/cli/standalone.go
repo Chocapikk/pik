b3b5ac9708b3acc33b2d330d02bee34d5510bf37
Valentin Lobstein <balgogan@protonmail.com>
2026-03-02 18:44:34 +0100

    Refactor: Unify CLI options with -s flag and fix RPORT injection

--- pkg/cli/standalone.go
+++ pkg/cli/standalone.go
@@ -4,13 +4,12 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/spf13/cobra"
 
-	"github.com/Chocapikk/pik/sdk"
 	"github.com/Chocapikk/pik/pkg/output"
 	"github.com/Chocapikk/pik/pkg/runner"
+	"github.com/Chocapikk/pik/sdk"
 )
 
 func init() {
@@ -25,20 +24,9 @@ func RunStandaloneWith(mod sdk.Exploit) {
 	}
 
 	var target, file, outputFile string
-	var threads int
+	var sets []string
+	var threads, targetIdx int
 	var checkOnly, jsonOutput bool
-
-	opts := sdk.ResolveOptions(mod)
-	defaults := make(map[string]string, len(opts))
-	flagVals := make(map[string]*string, len(opts))
-	for _, opt := range opts {
-		val := new(string)
-		*val = opt.Default
-		flagVals[opt.Name] = val
-		defaults[opt.Name] = opt.Default
-	}
-
-	var targetIdx int
 
 	cmd := &cobra.Command{
 		Use:           name,
@@ -58,8 +46,12 @@ func RunStandaloneWith(mod sdk.Exploit) {
 		},
 		RunE: func(_ *cobra.Command, _ []string) error {
 			ctx := context.Background()
-			params := flagParams(flagVals, defaults, target)
+			params := defaultParams(mod)
+			params.Set("TARGET", target)
 			params.Set("TARGET_INDEX", fmt.Sprintf("%d", targetIdx))
+			if err := parseOpts(sets, params); err != nil {
+				return err
+			}
 
 			if file != "" {
 				scan := &runner.Scanner{Module: mod, Targets: readTargets(file), Threads: threads, BaseParams: params, OutputFile: outputFile, JSONOutput: jsonOutput}
@@ -77,6 +69,7 @@ func RunStandaloneWith(mod sdk.Exploit) {
 	cmd.Flags().StringVarP(&target, "target", "t", "", "Target URL/IP")
 	cmd.Flags().StringVarP(&file, "file", "f", "", "File with targets")
 	cmd.Flags().StringVarP(&outputFile, "output", "o", "", "Output file")
+	cmd.Flags().StringArrayVarP(&sets, "set", "s", nil, "Set option (KEY=VALUE)")
 	cmd.Flags().IntVar(&threads, "threads", 10, "Threads")
 	cmd.Flags().BoolVar(&checkOnly, "check", false, "Check only")
 	cmd.Flags().BoolVar(&jsonOutput, "json", false, "JSON output (with -o)")
@@ -84,14 +77,6 @@ func RunStandaloneWith(mod sdk.Exploit) {
 	cmd.Flags().Bool("debug", false, "Debug")
 	if len(mod.Info().Targets) > 1 {
 		cmd.Flags().IntVar(&targetIdx, "exploit-target", 0, buildTargetFlag(mod))
-	}
-
-	for _, opt := range sdk.ResolveOptions(mod) {
-		flagName := strings.ToLower(strings.ReplaceAll(opt.Name, "_", "-"))
-		if cmd.Flags().Lookup(flagName) != nil {
-			continue
-		}
-		cmd.Flags().StringVar(flagVals[opt.Name], flagName, opt.Default, opt.Desc)
 	}
 
 	if err := cmd.Execute(); err != nil {
